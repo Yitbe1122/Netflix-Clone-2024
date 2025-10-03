e6e0866a69497d616fd70b813232b7a1b997ffd5
Yitbe1122 <yitbehiwot87@gmail.com>
2025-10-03 17:45:12 +0300

    header and footer added

---
+++ netflix-clone/src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './header.css'
+import NetflixLogo from '../../assets/Images/netflix-logo.png'
+import SearchIcon from '@mui/icons-material/Search';
+import  NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import Person2Icon from '@mui/icons-material/Person2';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+
+function Header() {
+  return (
+    <div className='header_outer_container'>
+        <div className='header_container'>
+            <div className='header_left'>
+                <ul>    
+                    <li><img src={NetflixLogo}  alt='Netflix Logo' width="100"/> </li>
+                    <li>Home</li>
+                    <li>TvShows</li>
+                    <li>Latest</li>
+                    <li>MyList</li>
+                    <li>Browse by Language</li>
+                </ul>
+            </div>
+            <div className='header_right'>
+                <ul>
+                    <li><SearchIcon/></li>
+                    <li><NotificationsNoneIcon /></li> 
+                    <li><Person2Icon/></li>
+                    <li><ArrowDropDownIcon/></li>
+                    {/* <li>Notifications</li> */}
+                    {/* <li>Profile</li> */}
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
